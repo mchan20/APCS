5ffc1edde611b35441ee0c748b8712815396d61b
mchan20 <mchan20@stuy.edu>
2020-10-22 23:04:24 -0400

    sum() finished, going to sleep now

---
+++ Tester.java
@@ -0,0 +1,14 @@
+public class Tester {
+  public static void main(String[] args) {
+    int[] arr0 = {1,2,3,4,5};
+    int[] arr1 = {10, -2, -1, 2, 3};
+    int[] arr2 = {410, -42, -1, 2, 3, 0, 0, 0, 12};
+
+    //sum() test cases
+    System.out.println(ArrayOps.sum(arr0));
+    System.out.println(ArrayOps.sum(arr1));
+    System.out.println(ArrayOps.sum(arr2));
+
+
+  }
+}
