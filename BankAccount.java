00bcd2590e3d84c4162c08b7051ac736347dc23b
mchan20 <mchan20@stuy.edu>
2020-10-27 21:51:49 -0400

    created a new tester file, deleted main method from class file

--- BankAccount.java
+++ BankAccount.java
@@ -13,8 +13,4 @@ public class BankAccount {
     return ("Your Balance is: " + balance);
   }
 
-  public static void main(String[] args) {
-    BankAccount b1 =  new BankAccount(12,"yomama");
-    System.out.println(b1.getBal());
-  }
 }
---
+++ BankTester.java
@@ -0,0 +1,6 @@
+public class BankTester {
+  public static void main(String[] args) {
+    BankAccount b1 =  new BankAccount(12,"yomama");
+    System.out.println(b1.getBal());
+  }
+}
