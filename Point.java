8cda4242800f1c4803fd981fd96f818f8aa21529
mchan20 <mchan20@stuy.edu>
2020-10-29 16:40:04 -0400

    finished class

--- Point.java
+++ Point.java
@@ -21,4 +21,17 @@ public class Point{
     return y;
   }
 
+  public static double distance(double x1, double y1, double x2, double y2) {
+    double xsquare = (x1-x2)*(x1-x2);
+    double ysquare = (y1-y2)*(y1-y2);
+    return Math.sqrt(xsquare+ysquare);
+  }
+
+  public double distanceTo(Point other){
+    return distance(getX(),getY(),other.getX(),other.getY());
+  }
+
+  public boolean equals(Point other) {
+    return ((getX() == other.getX()) && (getY() == other.getY()));
+  }
 }
