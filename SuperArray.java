d9482d896aade607be5eea93860c61a879731f0a
mchan20 <mchan20@stuy.edu>
2020-10-31 12:56:16 -0400

    created instance variables and constructors

---
+++ SuperArray.java
@@ -0,0 +1,12 @@
+public class SuperArray {
+  //Instance Variables
+  private String[] data;
+  private int size;
+
+  //Constructor
+  public SuperArray() {
+    size = 0;
+    data = new String[10];
+  }
+
+}
