438bdc0c7b7b188345220a2f4304b862ca6f4d74
mchan20 <mchan20@stuy.edu>
2020-12-04 16:10:48 -0500

    hasNext() done

--- Range.java
+++ Range.java
@@ -18,7 +18,9 @@ public class Range implements IntegerSequence{
   public int length(){
     return end - start + 1;
   }
-  public boolean hasNext(){  }
+  public boolean hasNext(){
+    return (current != end)
+  }
 
   //@throws NoSuchElementException
   public int next(){    }
