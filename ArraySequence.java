6da689c627b5f4280af8e8402d764ebf38bc61ae
mchan20 <mchan20@stuy.edu>
2020-12-07 13:58:21 -0500

    constructor done

--- ArraySequence.java
+++ ArraySequence.java
@@ -5,5 +5,11 @@ public class ArraySequence implements IntegerSequence{
   int []data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int [] other){
+    for(int i=0;i< other.length;i++) {
+      data[i] = other[i];
+    }
+  }
+
+
 }
