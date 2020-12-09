e40078363e1fb6686e8fc054afe4ce111b86b2ae
mchan20 <mchan20@stuy.edu>
2020-12-08 19:59:55 -0500

    length done

--- ArraySequence.java
+++ ArraySequence.java
@@ -14,7 +14,13 @@ public class ArraySequence implements IntegerSequence{
   boolean hasNext() {
     return (currentIndex != (data.length-1);
   }
-  int next();       //return the current value in the sequence and advances to the next element.
-  int length();     //returns the total length of the sequence
-  void reset();     //start over from the start of the sequence
+
+  int length() {
+    return data.length;
+  }     //returns the total length of the sequence
+  void reset() {
+    
+  };     //start over from the start of the sequence
+
+    int next() {}       //return the current value in the sequence and advances to the next element.
 }
