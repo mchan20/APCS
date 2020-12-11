faf736657991d6e92b6bcf6a5582509d32de8ddd
Michael <mchan20@stuy.edu>
2020-12-10 21:07:39 -0500

    second add method done

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -12,13 +12,18 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   }
 
   //Methods
-  public boolean add(T value) {
-    if (value.equals(null)) {
+  public boolean add(T element) {
+    if (element == null) {
       throw new IllegalArgumentException("You cannot add null to this list");
     }
-    super.add(value);
+    super.add(element);
     return true;
   }
 
-
+  public void add(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("You cannot add null to this list");
+    }
+    super.add(index, element);
+  }
 }
