7a2e6f87b2643e25f5f3fdfeb402381d83481121
Michael <mchan20@stuy.edu>
2020-12-10 20:46:13 -0500

    constructors done

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+
+
 public class NoNullArrayList<T> extends ArrayList<T> {
-  
+  //Contructors
+  public NoNullArrayList() {
+    super();
+  }
+
+  public NoNullArrayList(int initialCapacity) {
+    super(initialCapacity);
+  }
 }
