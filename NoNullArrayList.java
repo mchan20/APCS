ac134bacd3e4b7adc5398a459d4c42f495d2c4d8
Michael <mchan20@stuy.edu>
2020-12-10 21:14:52 -0500

    set method done

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -26,4 +26,11 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     }
     super.add(index, element);
   }
+
+  public T set(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("You cannot add null to this list");
+    }
+    super.set(index, element);
+  }
 }
