65c05b1a6552e0d3db5ea352f58d684c4c3e987e
Michael <mchan20@stuy.edu>
2020-12-10 22:43:32 -0500

    finished first add() fxn

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,4 +8,19 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(initialCapacity);
   }
 
+  //Methods
+  public boolean add(T element) {
+    int index = 0;
+    for(int i=0;i<size()-1;i++) {
+      if (get(i).compareTo(get(i+1)) < 0) {
+        index = i;
+        i = size()-1;
+      }
+    }
+    super.add(index,element);
+    return true;
+  }
+
+
+
 }
