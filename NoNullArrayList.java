12bce587c42b06b7d7684032dfec8234e540f2d9
Michael <mchan20@stuy.edu>
2020-12-10 23:22:28 -0500

    hw done

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -31,6 +31,8 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     if (element == null) {
       throw new IllegalArgumentException("You cannot add null to this list");
     }
+    T thing = get(index);
     super.set(index, element);
+    return thing;
   }
 }
