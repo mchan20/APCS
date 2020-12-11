f076255dab5f3fe67314c631b5827fa445e74887
Michael <mchan20@stuy.edu>
2020-12-10 22:46:23 -0500

    finished second add() fxn

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -21,6 +21,17 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     return true;
   }
 
+  public void add(int index, T element) {
+    int thing = 0;
+    for(int i=0;i<size()-1;i++) {
+      if (get(i).compareTo(get(i+1)) < 0) {
+        index = i;
+        i = size()-1;
+      }
+    }
+    super.add(thing,element);
+  }
+
 
 
 }
