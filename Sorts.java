8ae2216eff2ff576a8c5defaa73fd3407457a9d7
Michael <>
2020-12-21 14:39:58 -0500

    fixed selection sort

--- Sorts.java
+++ Sorts.java
@@ -1,6 +1,6 @@
 public class Sorts{
-  /**Bubble sort of an int array.
-  *@postcondition The array will be modified such that the elements will be in increasing order.
+  /**Bubble sort of an int dataay.
+  *@postcondition The dataay will be modified such that the elements will be in increasing order.
   *@param data  the elements to be sorted.
   */
   public static void bubbleSort(int[] data){
@@ -18,17 +18,14 @@ public class Sorts{
 
   public static void selectionSort(int[] data){
     int min = 0;
-    int swap = 0;
-    for(int i=0;i<data.length;i++) {
-      min = data[i];
-      for(int j=i;j<data.length;j++) {
-        if (min > data[j]) {
-          min = data[j];
-          swap = j;
-        }
+    for(int i=0;i<data.length-1;i++) {
+      int index = i;
+      for (int j=i+1;j<data.length;j++) {
+        if (data[j] < data[index]) index = j;
+        int temp = data[index];
+        data[index] = data[i];
+        data[i] = temp;
       }
-      data[swap] = data[i];
-      data[i] = min;
     }
   }
 
