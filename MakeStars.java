bb65b4a866a2e42146297aad278ca349471b261c
Michael <>
2021-01-04 15:59:32 -0500

    works for multiline files

--- MakeStars.java
+++ MakeStars.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class MakeStars {
   public static void main( String[]args ){
+    int count = 0;
     Scanner n = new Scanner( System.in );
     //use hasNextLine()/nextLine() to loop over
     //all of the data
@@ -9,6 +10,7 @@ public class MakeStars {
     //If you want to read the input word by word
     //this can be replaced with hasNext() and next()
     while (n.hasNextLine()) {
+      if (count > 0) System.out.println();
       Scanner thing = new Scanner( n.nextLine() );
       while (thing.hasNext()) {
         String temp = thing.next();
@@ -17,6 +19,7 @@ public class MakeStars {
         }
         System.out.print(" ");
       }
+      count++;
     }
   }
 }
