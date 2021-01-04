afdb2b44abf668ae8d3e6b0f27cc5f7217ca9205
Michael <>
2021-01-04 15:47:38 -0500

    code works for simple file

--- MakeStars.java
+++ MakeStars.java
@@ -2,12 +2,21 @@ import java.util.*;
 
 public class MakeStars {
   public static void main( String[]args ){
-      Scanner n = new Scanner( System.in );
-      //use hasNextLine()/nextLine() to loop over
-      //all of the data
+    Scanner n = new Scanner( System.in );
+    //use hasNextLine()/nextLine() to loop over
+    //all of the data
 
-      //If you want to read the input word by word
-      //this can be replaced with hasNext() and next()
-
+    //If you want to read the input word by word
+    //this can be replaced with hasNext() and next()
+    while (n.hasNextLine()) {
+      Scanner thing = new Scanner( n.nextLine() );
+      while (thing.hasNext()) {
+        String temp = thing.next();
+        for(int i=0;i<temp.length();i++) {
+          System.out.print("*");
+        }
+        System.out.print(" ");
+      }
+    }
   }
 }
