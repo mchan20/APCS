4554631d43255588e8c10e9fe23737461136108d
Michael <>
2021-01-04 15:43:20 -0500

    first commit

---
+++ MakeStars.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class MakeStars {
+  public static void main( String[]args ){
+      Scanner n = new Scanner( System.in );
+      //use hasNextLine()/nextLine() to loop over
+      //all of the data
+
+      //If you want to read the input word by word
+      //this can be replaced with hasNext() and next()
+
+  }
+}
