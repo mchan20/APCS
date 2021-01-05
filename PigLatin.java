8fff6f90a2829d4edb475dbea231c0d8e232538b
Michael <>
2021-01-04 22:00:58 -0500

    pigLatin best done

--- PigLatin.java
+++ PigLatin.java
@@ -34,16 +34,40 @@ public class PigLatin {
     return lala;
   }
 
-  public static void main(String[] args) {
-    System.out.println(pigLatinSimple("mock"));
-    System.out.println(pigLatinSimple("pie"));
-    System.out.println(pigLatinSimple("david"));
-    System.out.println(pigLatinSimple("aaron"));
+  public static String pigLatinBest(String s){
+    char last = s.charAt(s.length()-1);
+    char first = s.charAt(0);
+    if ((first < 97) || (first > 122)) {
+      return s;
+    }
+    else if ((last < 97) || (last > 122)) {
+      return pigLatin(s.substring(0,s.length()-1)) + last;
+    }
+    else {
+      return pigLatin(s);
+    }
 
-    System.out.println(pigLatin("the"));
-    System.out.println(pigLatin("check"));
-    System.out.println(pigLatin("skee"));
-    System.out.println(pigLatin("emu"));
-    System.out.println(pigLatin("grade"));
+  }
+
+  public static void main(String[] args) {
+    // System.out.println(pigLatinSimple("mock"));
+    // System.out.println(pigLatinSimple("pie"));
+    // System.out.println(pigLatinSimple("david"));
+    // System.out.println(pigLatinSimple("aaron"));
+    //
+    // System.out.println(pigLatin("the"));
+    // System.out.println(pigLatin("check"));
+    // System.out.println(pigLatin("skee"));
+    // System.out.println(pigLatin("emu"));
+    // System.out.println(pigLatin("grade"));
+
+    System.out.println(pigLatinBest("*emu"));
+    System.out.println(pigLatinBest("4chan"));
+    System.out.println(pigLatinBest("fish!"));
+    System.out.println(pigLatinBest("fish"));
+    System.out.println(pigLatinBest("the."));
+    System.out.println(pigLatinBest("cat!"));
+    System.out.println(pigLatinBest("amazing?"));
+    System.out.println(pigLatinBest("apple%"));
   }
 }
