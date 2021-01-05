c5c1c489eaa06e82c21b99cf0e23f6ebf296c7cf
Michael <>
2021-01-05 16:18:05 -0500

    linked list constructor done

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,12 +1,19 @@
 public class MyLinkedList{
- private int size;
- private Node start,end;
- public MyLinkedList(){/*create a constructor*/}
- public int size();
- public boolean add(String value);
- public boolean add(int index, String value);
- public String get(int index);
- public String set(int index, String value);
- public String toString();
- //Any helper method that returns a Node object MUST BE PRIVATE!
+  private int size;
+  private Node start,end;
+  public MyLinkedList(){
+   size = 0;
+   start = null;
+   end = null;
+  }
+  // public int size();
+  // public boolean add(String value);
+  // public boolean add(int index, String value);
+  // public String get(int index);
+  // public String set(int index, String value);
+  // public String toString();
+  // //Any helper method that returns a Node object MUST BE PRIVATE!
+  public static void main(String[] args) {
+    MyLinkedList hi = new MyLinkedList();
+  }
 }
