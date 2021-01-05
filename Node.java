0a5b59e1d026ba7e62452e61ee6b17bea7405380
Michael <>
2021-01-05 16:11:16 -0500

    node done

--- Node.java
+++ Node.java
@@ -1,12 +1,32 @@
 public class Node{
- public Node(String value){
-   data = value;
- }
- private String data;
- private Node next,prev;
- //write get/set methods for all three instance variables.
+  public Node(String value){
+    data = value;
+  }
+  private String data;
+  private Node next,prev;
 
- public String getData() {
-   return data;
- }
+  public String getData() {
+    return data;
+  }
+
+  public void setData(String value) {
+    data = value;
+  }
+
+  private Node getNext() {
+    return next;
+  }
+
+  private void setNext(Node other){
+    next = other;
+  }
+
+  private Node getPrev() {
+    return prev;
+  }
+
+  private void setPrev(Node other){
+    prev = other;
+  }
+
 }
