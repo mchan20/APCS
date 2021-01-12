f0fe6cd8b77d8a78f501d2f4b54eb39f9a712a99
Michael <>
2021-01-12 14:34:15 -0500

    merge done

--- Radix.java
+++ Radix.java
@@ -10,16 +10,31 @@ public class Radix {
   }
 
   public static void merge(MyLinkedList original,MyLinkedList[]buckets) {
-
+    for(MyLinkedList m : buckets) {
+      original.extend(m);
+    }
   }
+
   public static void main(String[] args) {
     // System.out.println(nth(123,1));
     // System.out.println(nth(-123,1));
     // System.out.println(nth(123,2));
     // System.out.println(nth(-123,2));
-    System.out.println(length(0));
-    System.out.println(length(15));
-    System.out.println(length(-10));
-    System.out.println(length(5112));
+    // System.out.println(length(0));
+    // System.out.println(length(15));
+    // System.out.println(length(-10));
+    // System.out.println(length(5112));
+    MyLinkedList thing[] = new MyLinkedList[4];
+    MyLinkedList hi = new MyLinkedList();
+    for(int i = 0;i<4;i++) {
+      MyLinkedList la = new MyLinkedList();
+      la.add(i+"");
+      thing[i] = la;
+    }
+    for(MyLinkedList m : thing) {
+      System.out.println(m.toString());
+    }
+    merge(hi,thing);
+    System.out.println(hi.toString());
   }
 }
