38ec279368ad55652e28490481098282c9dd2cd6
Michael <>
2021-01-12 10:26:26 -0500

    first commit

---
+++ Radix.java
@@ -0,0 +1,13 @@
+public class Radix {
+  public static int nth(int n, int col) {
+    return 0;
+  }
+
+  public static int length(int n) {
+    return 0;
+  }
+
+  public static void merge(MyLinkedList original,MyLinkedList[]buckets) {
+
+  }
+}
