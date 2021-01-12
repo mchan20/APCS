c41465083a26cdfd27088dc932c1790b52b98b59
Michael <>
2021-01-12 18:31:10 -0500

    works if there are both postitive and negative numbers

--- Radix.java
+++ Radix.java
@@ -30,12 +30,34 @@ public class Radix {
           buckets[0].add(lala);
         }
         else {
-        buckets[nth(lala,i)].add(lala);
+          buckets[nth(lala,i)].add(lala);
         }
+        if (length(lala) > maxCol) maxCol = length(lala);
       }
       clear(data);
       merge(data, buckets);
     }
+  }
+
+  public static void radixSort(SortableLinkedList data) {
+    int lala = 0;
+    int thing = 0;
+    SortableLinkedList negative = new SortableLinkedList();
+    SortableLinkedList positive = new SortableLinkedList();
+    for(int i=0;i<data.size();i++) {
+      lala = data.get(i);
+      if (lala < 0) negative.add(lala*-1);
+      else positive.add(lala);
+    }
+    clear(data);
+    radixSortSimple(positive);
+    if (negative.size() > 0) {
+      radixSortSimple(negative);
+      for(int i=negative.size()-1;i>=0;i--) {
+        data.add(negative.get(i)*-1);
+      }
+    }
+    data.extend(positive);
   }
 
   public static void clear(SortableLinkedList list) {
@@ -46,13 +68,15 @@ public class Radix {
 
   public static void main(String[] args) {
     SortableLinkedList hi = new SortableLinkedList();
-    hi.add(111);
+    hi.add(0);
     hi.add(271);
-    hi.add(71);
-    hi.add(151);
+    hi.add(-71);
+    hi.add(-72);
+    hi.add(-74);
+    hi.add(-111);
     hi.add(91);
     System.out.println(hi.toString());
-    radixSortSimple(hi);
+    radixSort(hi);
     System.out.println(hi.toString());
   }
 }
