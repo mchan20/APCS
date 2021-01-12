b94b3ad9d59668b0592e2b14b8d4fd4036dc97d6
Michael <>
2021-01-12 14:02:48 -0500

    length done

--- Radix.java
+++ Radix.java
@@ -5,16 +5,21 @@ public class Radix {
   }
 
   public static int length(int n) {
-    return 0;
+    String result = ""+Math.abs(n);
+    return result.length();
   }
 
   public static void merge(MyLinkedList original,MyLinkedList[]buckets) {
 
   }
   public static void main(String[] args) {
-    System.out.println(nth(123,1));
-    System.out.println(nth(-123,1));
-    System.out.println(nth(123,2));
-    System.out.println(nth(-123,2));
+    // System.out.println(nth(123,1));
+    // System.out.println(nth(-123,1));
+    // System.out.println(nth(123,2));
+    // System.out.println(nth(-123,2));
+    System.out.println(length(0));
+    System.out.println(length(15));
+    System.out.println(length(-10));
+    System.out.println(length(5112));
   }
 }
