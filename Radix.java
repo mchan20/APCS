1ac306f23ac1861f908995eebe4175d9797c4d88
Michael <>
2021-01-12 13:56:00 -0500

    nth done

--- Radix.java
+++ Radix.java
@@ -1,6 +1,7 @@
 public class Radix {
   public static int nth(int n, int col) {
-    return 0;
+    n = Math.abs(n);
+    return ( n / (int) Math.pow(10,col) ) % 10;
   }
 
   public static int length(int n) {
@@ -9,5 +10,11 @@ public class Radix {
 
   public static void merge(MyLinkedList original,MyLinkedList[]buckets) {
 
+  }
+  public static void main(String[] args) {
+    System.out.println(nth(123,1));
+    System.out.println(nth(-123,1));
+    System.out.println(nth(123,2));
+    System.out.println(nth(-123,2));
   }
 }
