f8f76f3c54346bfb1a7bdd0155946947b0c38be6
Michael <>
2021-01-12 17:52:28 -0500

    radix sort done

--- Radix.java
+++ Radix.java
@@ -11,46 +11,48 @@ public class Radix {
 
   public static void merge( SortableLinkedList original, SortableLinkedList[]buckets) {
     for(SortableLinkedList m : buckets) {
-      original.extend(m);
+      if (m != null) original.extend(m);
     }
   }
 
   public static void radixSortSimple(SortableLinkedList data) {
     int lala = 0;
     SortableLinkedList[] buckets = new SortableLinkedList[10];
-    int maxCol = length(data.get(0));
+    for (int i=0;i<10;i++) {
+      buckets[i] = new SortableLinkedList();
+    }
     SortableLinkedList temp = new SortableLinkedList();
+    int maxCol = length(data.get(0));
     for (int i=0;i<maxCol;i++) {
       for (int j=0;j<data.size();j++) {
         lala = data.get(j);
-        temp.add(lala);
-        buckets[nth(lala,i)].extend(temp);
+        if (lala < Math.pow(10,i)) {
+          buckets[0].add(lala);
+        }
+        else {
+        buckets[nth(lala,i)].add(lala);
+        }
       }
+      clear(data);
       merge(data, buckets);
     }
   }
 
+  public static void clear(SortableLinkedList list) {
+    SortableLinkedList temp = new SortableLinkedList();
+    temp.extend(list);
+  }
 
-  // public static void main(String[] args) {
-  //   System.out.println(nth(123,1));
-  //   System.out.println(nth(-123,1));
-  //   System.out.println(nth(123,2));
-  //   System.out.println(nth(-123,2));
-  //   System.out.println(length(0));
-  //   System.out.println(length(15));
-  //   System.out.println(length(-10));
-  //   System.out.println(length(5112));
-  //   MyLinkedList thing[] = new MyLinkedList[4];
-  //   MyLinkedList hi = new MyLinkedList();
-  //   for(int i = 0;i<4;i++) {
-  //     MyLinkedList la = new MyLinkedList();
-  //     la.add(i+"");
-  //     thing[i] = la;
-  //   }
-  //   for(MyLinkedList m : thing) {
-  //     System.out.println(m.toString());
-  //   }
-  //   merge(hi,thing);
-  //   System.out.println(hi.toString());
-  // }
+
+  public static void main(String[] args) {
+    SortableLinkedList hi = new SortableLinkedList();
+    hi.add(111);
+    hi.add(271);
+    hi.add(71);
+    hi.add(151);
+    hi.add(91);
+    System.out.println(hi.toString());
+    radixSortSimple(hi);
+    System.out.println(hi.toString());
+  }
 }
